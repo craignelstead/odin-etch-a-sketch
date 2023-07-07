1ac0268bb64fdae5674b3af67c876911343c8875
craignelstead <ctnelst92@gmail.com>
2023-07-07 14:28:40 -0600

    Added custom input to set grid size

--- script.js
+++ script.js
@@ -5,45 +5,66 @@
 //Calls setNewGrid when page loads
 window.addEventListener('load', () => {
     setNewGrid();
-    drawOnDivs();
+    assignBtnListeners();
 });
 
 //Assign event listeners to buttons
-let clearBtn = document.getElementById('cleardrawing');
-clearBtn.addEventListener('click', clearGrid);
+function assignBtnListeners() {
+    let clearBtn = document.getElementById('cleardrawing');
+    clearBtn.addEventListener('click', clearDrawing);
+
+    let newGridBtn = document.getElementById('changegridsize');
+    newGridBtn.addEventListener('click', clearAndMakeNew);
+}
+
+function clearAndMakeNew() {
+    clearDrawing();
+    setNewGrid();
+}
 
 //Creates grid in content area by getting gridSize and using a loop to add divs
 function setNewGrid() {
+    //Deletes any exisiting grid
+    deleteGrid();
+
     //Get the grid size
-    //let gridInput = document.getElementById('#inputgridnum').value;
-    gridSize = 16;
+    let gridSize = document.getElementById('inputgridnum').value;
 
-    //Variable for the content container (where the grid will live)
-    const gridContainer = document.querySelector('#contentcontainer');
+    //Validate input
+    if (checkIfValidInput(gridSize) === false) {
+        alert('Please enter a valid number');
+        document.getElementById('inputgridnum').focus();
+        document.getElementById('inputgridnum').select();
+    }
+    else {
 
-    //This loop will create a container column that has gridSize number of divs
-    //It will then add it to the content area
-    //While the counter i is less than gridSize, the loop will continue to add
-    //more columns
+        //Variable for the content container (where the grid will live)
+        const gridContainer = document.querySelector('#contentcontainer');
 
-    for (let i=0; i < gridSize; i++) {
-        const newLine = document.createElement('div');
-        newLine.classList.add('rowdiv');
-        gridContainer.appendChild(newLine);
+        //This loop will create a container column that has gridSize number of divs
+        //It will then add it to the content area
+        //While the counter i is less than gridSize, the loop will continue to add
+        //more columns
 
-        for (let j=0; j < gridSize; j++) {
-            const latestRow = gridContainer.lastElementChild;
+        for (let i=0; i < gridSize; i++) {
+            const newLine = document.createElement('div');
+            newLine.classList.add('rowdiv');
+            gridContainer.appendChild(newLine);
 
-            const newBox = document.createElement('div');
-            newBox.classList.add('griddiv');
+            for (let j=0; j < gridSize; j++) {
+                const latestRow = gridContainer.lastElementChild;
 
-            newBox.textContent = j + 1;
+                const newBox = document.createElement('div');
+                newBox.classList.add('griddiv');
 
-            latestRow.appendChild(newBox);
+                latestRow.appendChild(newBox);
+            }
+
         }
 
+        //Add event listeners to draw on divs
+        drawOnDivs();
     }
-    
 }
 
 //Changes griddiv background color to black when mouse enters it
@@ -59,11 +80,33 @@ function drawOnDivs() {
 }
 
 //Sets background color of all divs to white
-function clearGrid() {
+function clearDrawing() {
     const drawingDiv = document.getElementsByClassName('griddiv');
 
     //Goes through array of all griddiv class and assigns each an event listener
     for (let i=0; i<drawingDiv.length; i++) {
             drawingDiv[i].style.backgroundColor = 'white';
+    }
+}
+
+//Deletes existing grid divs
+function deleteGrid() {
+    let existingDivs = document.querySelectorAll('.rowdiv');
+    existingDivs.forEach(e => e.remove());
+}
+
+function checkIfValidInput(input) {
+    let textBox = document.querySelector('#inputgridnum');
+
+    //Determine if input is integer between 1 and 100
+    if (input >= 1 && input <= 100 && (Number.isInteger(Number(input)))) {
+        textBox.style.backgroundColor = 'white';
+        textBox.style.color = 'black';
+        return true;
+    }
+    else {
+        textBox.style.backgroundColor = 'red';
+        textBox.style.color = 'white';
+        return false;
     }
 }
